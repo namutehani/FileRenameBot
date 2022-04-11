d2e47000030d2e68bb8db4c2280046740c8e2607
namutehani <58953928+namutehani@users.noreply.github.com>
2022-04-11 21:30:39 +0300

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -36,7 +36,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["c2v"]))
+@Mai_bOTs.on_message(pyrogram.filters.command(["c2v"]) & pyrogram.filters.user(Config.AUTH_USERS))
 async def convert_to_video(bot, update):
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
