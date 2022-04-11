0e157f11f84749cd5550ee6f0c0f92eb70128a41
namutehani <58953928+namutehani@users.noreply.github.com>
2022-04-11 21:38:36 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -13,7 +13,9 @@ class Config(object):
     # Get these values from my.telegram.org
     CHAT_ID = os.environ.get("CHAT_ID", "")
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = []
+    AUTH_USER = AUTH_USERS.append(int(x)) for x in os.environ.get("AUTH_USERS", "").split())
+    
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
