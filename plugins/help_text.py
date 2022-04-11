134ab670037fac9cc9c480afe9665285b58c3d3b
namutehani <58953928+namutehani@users.noreply.github.com>
2022-04-11 21:13:45 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -81,21 +81,6 @@ async def start_me(bot, update):
         return
     update_channel = Config.UPDATE_CHANNEL
     await update.reply_text(Translation.START_TEXT.format(update.from_user.first_name),
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                        InlineKeyboardButton("Help", callback_data = "ghelp")
-                ],
-                [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
-                ],
-                [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
-                ]
-            ]
-        ),
         reply_to_message_id=update.message_id
     )
 
