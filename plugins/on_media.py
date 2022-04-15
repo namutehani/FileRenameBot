21bab852dc143e80e433ec5fdbdd02e3f1ab0711
namutehani <58953928+namutehani@users.noreply.github.com>
2022-04-15 12:01:59 +0300

    Create on_media.py

---
+++ plugins/on_media.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from pyrogram.types import Message
+from bot import Bot
+
+@Bot.on_message((filters.video | filters.audio | filters.document) & ~filters.channel & ~filters.edited)
+async def on_media_handler(c: Client, m: Message):
+    name = m.document.file_name
+    if not m.from_user:
+        return await m.reply_text("I don't know about you sar :(")
+    await asyncio.sleep(3)
+    await m.reply_text(name,quote=True)
