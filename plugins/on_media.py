caebbbd54d0f6bcbbedbb24b6e1cf66c0b3d4ede
namutehani <58953928+namutehani@users.noreply.github.com>
2022-04-16 13:14:48 +0300

    Update on_media.py

--- plugins/on_media.py
+++ plugins/on_media.py
@@ -6,6 +6,8 @@ import asyncio
 @Client.on_message((filters.video | filters.audio | filters.document) & ~filters.channel & ~filters.edited)
 async def on_media_handler(c: Client, m: Message):
     name = m.document.file_name
+    name1 = name.replace(".pdf"," @tayfaykspdf .pdf")
     if not m.from_user:
         return await m.reply_text("I don't know about you sar :(")
     await m.reply_text(f"`{name}`",parse_mode="markdown",quote=True)
+    m.reply_text(f"/rename `{name1}`",parse_mode="markdown",quote=True)
